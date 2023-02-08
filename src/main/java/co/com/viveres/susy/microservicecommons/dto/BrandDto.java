2b2bff2f7db2d14ff440459f2c919d7d4a393265
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2023-02-08 18:35:20 -0500

    Update BrandDto.java

--- src/main/java/co/com/viveres/susy/microservicecommons/dto/BrandDto.java
+++ src/main/java/co/com/viveres/susy/microservicecommons/dto/BrandDto.java
@@ -1,25 +1,24 @@
 package co.com.viveres.susy.microservicecommons.dto;
 
-import java.io.Serializable;
-
-import javax.validation.constraints.NotNull;
-
 import co.com.viveres.susy.microservicecommons.validation.ICreateProductValidation;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
 @Data
 @ApiModel(value = "Brand")
 public class BrandDto implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	@NotNull(groups = ICreateProductValidation.class)
-	@ApiModelProperty(notes = "Brand Id", example = "1")
-	private Long id;
-	
-	@ApiModelProperty(notes = "Brand name", example = "Diana")
-	private String name;
+    private static final long serialVersionUID = 1L;
+
+    @NotNull(groups = ICreateProductValidation.class)
+    @ApiModelProperty(notes = "Brand Id", example = "1")
+    private Long id;
+
+    @ApiModelProperty(notes = "Brand name", example = "Diana")
+    private String name;
 
 }
